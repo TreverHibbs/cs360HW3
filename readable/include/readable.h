267e904753261bd1346fdcedf2b4945012684647
treverhibbs <treverhibbs@gmail.com>
2020-02-16 13:33:59 -0800

    Edit readable function to be able to get $PWD
    Why: To allow the readable recursive function to get current
    directory if no directory is given.

--- readable/include/readable.h
+++ readable/include/readable.h
@@ -1,1 +1,5 @@
+/* Function meant to travers a directory and count the number
+of readable files. */
 int readable(char *inputPath);
+
+void deleteBuffers(int length, char **buffers);
--- readable/readable.c
+++ readable/readable.c
@@ -1,4 +1,33 @@
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+
+void deleteBuffers(int length, char **buffers){
+    int i = 0;
+    while(i < length){
+        free(buffers[i]);
+        i++;
+    }
+    return;
+}
 
 int readable(char *inputPath){
+    int length_of_buffer_array = 0;
+    char **buffer_array = calloc(10, sizeof(char**));
+
+    if(inputPath == NULL){
+        inputPath = get_current_directory_name();
+        if(inputPath == NULL){
+            fprintf (stderr, "%s: Can't access PWD variable for path --%s --errno=%d\n"
+                     ,"readable" , strerror(errno), errno);
+            return(-1);
+        }
+        buffer_array[length_of_buffer_array++] = inputPath;
+    }
+
+    //create base case for recursive call.
+
+
     return(1);
 }
