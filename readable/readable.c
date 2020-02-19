8df134f1a6077651a1f94941ea62127268c4cfe6
treverhibbs <treverhibbs@gmail.com>
2020-02-18 19:26:29 -0800

    set return values for errors and refactor code
    See: n/a
    
    Why: To make code more readable and also to make sure that when errors
    are encountered in recursive calls they do not effect the readable file
    count
    
    How: n/a

--- readable/readable.c
+++ readable/readable.c
@@ -37,18 +37,26 @@ void closeAndFreeDirp(DIR *dirp){
 }
 
 int readable(char *input_path){
-    int length_of_buffer_array = 0;
+    errno = 0;
+    
+    //array for storing buffers for memory management.
     void **buffer_array = calloc(10, sizeof(void**));
+    int length_of_buffer_array = 0;
+    
+    //variables and pointers for function return values.
     int access_return = 1;
     int stat_return = 0;
-    int readable_files = 0;
-    DIR *dirp = NULL;
+    int tmp = 0;
     struct dirent *my_dirent = NULL;
-    errno = 0;
-    char *file_path = NULL;
-    char *new_file_path = NULL;
+    DIR *dirp = NULL;
+
     struct stat* stat_buf = malloc(sizeof(struct stat));
     buffer_array[length_of_buffer_array++] = stat_buf;
+    
+    int readable_files = 0;
+    
+    char *file_path = NULL;
+    char *new_file_path = NULL;
 
     if(input_path == NULL){
         input_path = malloc(PATH_MAX);
@@ -57,7 +65,7 @@ int readable(char *input_path){
                      ,"readable" , strerror(errno), errno);
             deleteBuffers(length_of_buffer_array, buffer_array);
             closeAndFreeDirp(dirp);
-            return(-1);
+            return(-errno);
         }
         input_path = getcwd(input_path, PATH_MAX);
         if(input_path == NULL){
@@ -65,40 +73,50 @@ int readable(char *input_path){
                      ,"readable" , strerror(errno), errno);
             deleteBuffers(length_of_buffer_array, buffer_array);
             closeAndFreeDirp(dirp);
-            return(-1);
+            return(-errno);
         }
         buffer_array[length_of_buffer_array++] = input_path;
     }
-
-    printf("input_path is %s\n", input_path);
 
     stat_return = stat(input_path, stat_buf);
     if(stat_return < 0){
         fprintf (stderr, "%s: Can't get status of file with path %s --%s --errno=%d\n"
                  ,"readable" , input_path, strerror(errno), errno);
         deleteBuffers(length_of_buffer_array, buffer_array);
-        return(readable_files);
+        return(-errno);
     }
 
     access_return = access(input_path, R_OK);
     if(access_return < 0){
-        fprintf (stderr, "%s: Can't access file with path %s --%s --errno=%d\n"
+        fprintf (stderr, "%s: Can't read file with path %s --%s --errno=%d\n"
                  ,"readable" , input_path, strerror(errno), errno);
         deleteBuffers(length_of_buffer_array, buffer_array);
-        return(readable_files);
+        return(-errno);
     }
 
+    //check if file specified by input_path is regular
+    //and readable. If it is regular but not readable
+    //and if it is a symbolic link.
     if(S_ISREG(stat_buf->st_mode) 
        && access_return == 0){
         
         deleteBuffers(length_of_buffer_array, buffer_array);
         readable_files++;
         return(readable_files); 
-    }else if(access_return = access(input_path, X_OK), access_return < 0){
-        fprintf (stderr, "%s: Can't access directory named %s --%s --errno=%d\n"
+    }else if(S_ISREG(stat_buf->st_mode)){
+        deleteBuffers(length_of_buffer_array, buffer_array);
+        return(readable_files); 
+    }else if(S_ISLNK(stat_buf->st_mode)){
+        deleteBuffers(length_of_buffer_array, buffer_array);
+        return(readable_files); 
+    }
+    
+    //check if file specified by input_path is executable
+    if(access_return = access(input_path, X_OK), access_return < 0){
+        fprintf (stderr, "%s: Can't execute directory named %s --%s --errno=%d\n"
                  ,"readable" , input_path, strerror(errno), errno);
         deleteBuffers(length_of_buffer_array, buffer_array);
-        return(readable_files);
+        return(-errno);
     }else{
         dirp = opendir(input_path);
         if(dirp == NULL){
@@ -111,7 +129,6 @@ int readable(char *input_path){
     }
 
     while(my_dirent = readdir(dirp), my_dirent != NULL){
-        printf("my_dirent->d_name is %s\n", my_dirent->d_name);
         new_file_path = malloc(PATH_MAX);
 
         file_path = malloc(PATH_MAX);
@@ -127,22 +144,16 @@ int readable(char *input_path){
                  && strcmp(my_dirent->d_name, ".") != 0){
             
             strcpy(new_file_path, file_path);
-            readable_files = readable_files + readable(new_file_path);
+            tmp = readable(new_file_path);
+
+            if(tmp > 0){
+                readable_files = readable_files + readable(new_file_path);
+            }
         }
 
         free(file_path);
         free(new_file_path);
         file_path = NULL;
-    }
-
-    //check if readdir errored
-    if(errno != 0){
-        fprintf (stderr, "%s: Can't read dirp --%s --errno=%d\n"
-                 ,"readable", strerror(errno), errno);
-
-        deleteBuffers(length_of_buffer_array, buffer_array);
-        closeAndFreeDirp(dirp);
-        return(readable_files);
     }
 
     deleteBuffers(length_of_buffer_array, buffer_array);
